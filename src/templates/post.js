d24fa7656491c5b37a761387d72196cebba52bea
raythurman2386 <raymondthurman5@gmail.com>
2019-07-14 19:58:10 -0400

    updated pages

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,6 +1,26 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 import Helmet from 'react-helmet';
 
 export default function Template({ data }) {
 	const { markdownRemark: post } = data;
+	// same as
+	// const post = data.markdownRemark;
+	return (
+		<div>
+			<h1>{post.frontmatter.title}</h1>
+		</div>
+	);
 }
+
+export const postQuery = graphql`
+	query BlogPostByPath($path: String!) {
+		markdownRemark(frontmatter: { path: { eq: $path } }) {
+			html
+			frontmatter {
+				path
+				title
+			}
+		}
+	}
+`;
